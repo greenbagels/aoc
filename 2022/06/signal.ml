e4c09d030eff64d4145f5a1a39959c7f1884029c
Sameed Pervaiz <pervaiz.8@osu.edu>
2022-12-06 01:32:18 -0500

    Style: reformat Day 6 using ocamlformat

--- 2022/06/signal.ml
+++ 2022/06/signal.ml
@@ -1,27 +1,19 @@
-
-type part =
-| One
-| Two
+type part = One | Two
 
 let calc_solution part =
   let rec find_nonrepeat str idx =
-    let k =
-      match part with
-      | One -> 4
-      | Two -> 14
-    in
+    let k = match part with One -> 4 | Two -> 14 in
     let tbl = Hashtbl.create k in
     let () =
-    for i = 0 to (k-1) do
-      match Hashtbl.find_opt tbl str.[idx + i] with
-      | None -> Hashtbl.add tbl str.[idx + i] 1
-      | Some n -> Hashtbl.replace tbl str.[idx + i] (n + 1)
-    done in
+      for i = 0 to k - 1 do
+        match Hashtbl.find_opt tbl str.[idx + i] with
+        | None -> Hashtbl.add tbl str.[idx + i] 1
+        | Some n -> Hashtbl.replace tbl str.[idx + i] (n + 1)
+      done
+    in
     match Hashtbl.fold (fun k v init -> v * init) tbl 1 with
-    | 1 ->
-        idx + k
-    | n ->
-        find_nonrepeat str (idx + 1)
+    | 1 -> idx + k
+    | n -> find_nonrepeat str (idx + 1)
   in
   let ic = open_in "input" in
   let input_str = input_line ic in
